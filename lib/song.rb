4a4fffaa22b0808e53e4004a96420135760dbf2b
J. Singleton <jennifer.singleton@gmail.com>
2019-02-02 18:36:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Song
   extend Memorable
   extend Findable
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
@@ -12,10 +13,6 @@ class Song
   def initialize
     @@songs << self
   end
-
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
 
   def self.all
     @@songs
